26516ab277257ac9725ee477457f9a6e7b96c89f
Michael Farrant <michaelfarrant@outlook.com>
2024-09-14 16:35:12 +0100

    Added node traversal to find the valid combinations of elements
    Needs a tidy!

--- src/ElementalWords/Program.cs
+++ src/ElementalWords/Program.cs
@@ -1,22 +1,63 @@
-﻿
-
-using ElementalWords;
+﻿using ElementalWords;
 
 
+    var periodicTable = new PeriodicTable([
+        new("S", "Sulfur"),
+        new("N", "Nitrogen"),
+        new("Ac", "Actinium"),
+        new("K", "Potassium"),
+        new("Na","Sodium"),
+        new("C", "Carbon"),
+        new("Sn", "Tin")
+    ]);
 
-ElementalWords("abc");
+var paths = ElementalWords("snack");
 
-void ElementalWords(string word)
+
+List<Element[]> ElementalWords(string word)
 {
 
     var trie = Trie.FromString(word, 3);
-    var periodicTable = new PeriodicTable([
-        new("Fe", "Iron")
-    ]);
 
     //todo - add traversal of the trie
+    
+    var root = trie._root;
+
+    var validPaths = new List<Element[]>();
 
 
+    foreach(Node n in root.Nodes.Values){
 
+        var route = new List<Node>();
+        NavigateNode(n, route, validPaths);
+    }
+
+    return validPaths;
 }
 
+
+void NavigateNode(Node node, List<Node> route, List<Element[]> validPaths){
+    route.Add(node);
+    
+    if(!periodicTable.HasElement(node.Prefix)){
+        // prefix is not an element, bail out as what ever follows can never create an elemental word!
+        return;
+    }
+
+
+    if(node.IsLeaf){
+        // reached a leaf, reached the end. Must be a valid combo
+        var path = route.Select(n => periodicTable.GetElement(n.Prefix)).ToArray();
+        validPaths.Add(path);
+
+    }
+    else{
+
+        foreach(Node n in node.Nodes.Values){
+            NavigateNode(n, route, validPaths);
+            route.RemoveAt(route.Count -1);
+        }
+
+    }
+
+}
