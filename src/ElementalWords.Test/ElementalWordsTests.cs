bac105e7b69c088c78d9a0a5912c547a3bb68462
Michael Farrant <michaelfarrant@outlook.com>
2024-09-14 17:32:19 +0100

    Added test on ElementalForms, imported from Kata task definition

---
+++ src/ElementalWords.Test/ElementalWordsTests.cs
@@ -0,0 +1,48 @@
+﻿namespace ElementalWords.Test
+{
+    [TestFixture]
+    public class ElementalWordsTests
+    {
+
+
+        [Test]
+        public void SimpleCases()
+        {
+
+            var periodicTable = new PeriodicTable([
+                new("S", "Sulfur"),
+                new("N", "Nitrogen"),
+                new("Ac", "Actinium"),
+                new("K", "Potassium"),
+                new("Na", "Sodium"),
+                new("C", "Carbon"),
+                new("Sn", "Tin"),
+                new("Be", "Beryllium"),
+                new("H", "Hydrogen"),
+                new("Y", "Yttrium"),
+                new("Es", "Einsteinium")
+            ]);
+
+            var elementalWords = new ElementalWords(periodicTable);
+
+            // Testcases from kata task definiton
+            var cases = new[] {
+                ("", new string[0][]),
+                ("Yes", new[] { new[] {"Yttrium (Y)", "Einsteinium (Es)"} }),
+                ("beach", new[] { new[] {"Beryllium (Be)", "Actinium (Ac)", "Hydrogen (H)"} }),
+                ("snack", new[] {
+                    new[] {"Sulfur (S)", "Nitrogen (N)", "Actinium (Ac)", "Potassium (K)"},
+                    new[] {"Sulfur (S)", "Sodium (Na)", "Carbon (C)", "Potassium (K)"},
+                    new[] {"Tin (Sn)", "Actinium (Ac)", "Potassium (K)"}
+                }),
+            };
+
+            foreach (var (word, expected) in cases)
+            {
+                var actual = elementalWords.ElementalForms(word);
+                Assert.That(actual, Is.EquivalentTo(expected), $"Unexpected result for word: {word}");
+            }
+        }
+
+    }
+}
