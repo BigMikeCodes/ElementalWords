96559efae5c29f778be7e8f325bfc6564fcfec5a
Michael Farrant <michaelfarrant@outlook.com>
2024-09-14 16:33:57 +0100

    Added prop on node to determine if its a leaf

--- src/ElementalWords/Node.cs
+++ src/ElementalWords/Node.cs
@@ -6,10 +6,19 @@
         public string Prefix { get; set; } = string.Empty;
         public IDictionary<string, Node> Nodes { get; set; } = new Dictionary<string, Node>();
 
+        // No children == leaf node at the bottom of the trie
+        public bool IsLeaf => !Nodes.Any();
        
         public void AddChild(Node node)
         {
             Nodes.Add(node.Prefix, node);
+        }
+
+        public override string ToString()
+        {
+
+            var nodesString = string.Join(", ", Nodes.Values.Select(n => n.Prefix));
+            return $"[ Prefix = {Prefix}, Nodes = [{nodesString}] ]";
         }
 
     }
