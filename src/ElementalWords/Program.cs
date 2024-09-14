c557837250cadb15589016af2ba950daa7dbcfc0
Michael Farrant <michaelfarrant@outlook.com>
2024-09-14 17:58:15 +0100

    Added all elements to the periodic table
    Removed need to pass in elements to the table

--- src/ElementalWords/Program.cs
+++ src/ElementalWords/Program.cs
@@ -2,15 +2,7 @@
 
 var word = "snack";
 
-var periodicTable = new PeriodicTable([
-    new("S", "Sulfur"),
-    new("N", "Nitrogen"),
-    new("Ac", "Actinium"),
-    new("K", "Potassium"),
-    new("Na","Sodium"),
-    new("C", "Carbon"),
-    new("Sn", "Tin")
-]);
+var periodicTable = new PeriodicTable();
 
 var elementalWords = new ElementalWords.ElementalWords(periodicTable);
 var results = elementalWords.ElementalForms(word);
