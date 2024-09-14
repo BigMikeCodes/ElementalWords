951c6dd76b02d4402a37c4d67cd15743bc6690de
Michael Farrant <michaelfarrant@outlook.com>
2024-09-14 17:07:40 +0100

    Chore: moved the implementation to a seperate class to clean up program.cs & make it easier to test

---
+++ src/ElementalWords/ElementalWords.cs
@@ -0,0 +1,66 @@
+﻿namespace ElementalWords
+{
+    public class ElementalWords
+    {
+
+        private readonly PeriodicTable _periodicTable;
+
+
+        public ElementalWords(PeriodicTable periodicTable)
+        {
+            _periodicTable = periodicTable;
+        }
+
+
+        public IEnumerable<Element[]> ElementalForms(string word)
+        {
+            var trie = Trie.FromString(word, 3);
+
+            var validPaths = new List<Element[]>();
+
+
+            foreach (Node n in trie.Root.Nodes.Values)
+            {
+
+                var route = new List<Node>();
+                NavigateNode(n, route, validPaths);
+            }
+
+            return validPaths;
+
+        }
+
+        void NavigateNode(Node node, List<Node> route, List<Element[]> validPaths)
+        {
+            route.Add(node);
+
+            if (!_periodicTable.HasElement(node.Prefix))
+            {
+                // prefix is not an element, bail out as what ever follows can never create an elemental word!
+                return;
+            }
+
+
+            if (node.IsLeaf)
+            {
+                // reached a leaf, reached the end. Must be a valid combo
+                var path = route.Select(n => _periodicTable.GetElement(n.Prefix)).ToArray();
+                validPaths.Add(path);
+
+            }
+            else
+            {
+
+                foreach (Node n in node.Nodes.Values)
+                {
+                    NavigateNode(n, route, validPaths);
+                    route.RemoveAt(route.Count - 1);
+                }
+
+            }
+
+        }
+
+
+    }
+}
--- src/ElementalWords/Program.cs
+++ src/ElementalWords/Program.cs
@@ -1,58 +1,15 @@
 ﻿using ElementalWords;
 
 
-    var periodicTable = new PeriodicTable([
-        new("S", "Sulfur"),
-        new("N", "Nitrogen"),
-        new("Ac", "Actinium"),
-        new("K", "Potassium"),
-        new("Na","Sodium"),
-        new("C", "Carbon"),
-        new("Sn", "Tin")
-    ]);
+var periodicTable = new PeriodicTable([
+    new("S", "Sulfur"),
+    new("N", "Nitrogen"),
+    new("Ac", "Actinium"),
+    new("K", "Potassium"),
+    new("Na","Sodium"),
+    new("C", "Carbon"),
+    new("Sn", "Tin")
+]);
 
-var paths = ElementalWords("snack");
-
-
-List<Element[]> ElementalWords(string word)
-{
-
-    var trie = Trie.FromString(word, 3);
-    var validPaths = new List<Element[]>();
-
-
-    foreach(Node n in trie.Root.Nodes.Values){
-
-        var route = new List<Node>();
-        NavigateNode(n, route, validPaths);
-    }
-
-    return validPaths;
-}
-
-
-void NavigateNode(Node node, List<Node> route, List<Element[]> validPaths){
-    route.Add(node);
-    
-    if(!periodicTable.HasElement(node.Prefix)){
-        // prefix is not an element, bail out as what ever follows can never create an elemental word!
-        return;
-    }
-
-
-    if(node.IsLeaf){
-        // reached a leaf, reached the end. Must be a valid combo
-        var path = route.Select(n => periodicTable.GetElement(n.Prefix)).ToArray();
-        validPaths.Add(path);
-
-    }
-    else{
-
-        foreach(Node n in node.Nodes.Values){
-            NavigateNode(n, route, validPaths);
-            route.RemoveAt(route.Count -1);
-        }
-
-    }
-
-}
+var elementalWords = new ElementalWords.ElementalWords(periodicTable);
+var result = elementalWords.ElementalForms("snack");
